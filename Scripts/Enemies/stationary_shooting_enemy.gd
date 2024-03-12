47d15ff25f6017c8913ec706ff26bc5c81c5b457
Will <55513273+Whullum@users.noreply.github.com>
2024-03-12 16:04:19 -0400

    edited animation values and fixed anim bugs

--- Scripts/Enemies/stationary_shooting_enemy.gd
+++ Scripts/Enemies/stationary_shooting_enemy.gd
@@ -30,7 +30,7 @@ func _angle_towards_player():
 	pass
 
 func _on_detection_area_area_entered(area):
-	if area.name == "PlayerHitbox":
+	if area.name == "PlayerHitbox" and health > 0:
 		#print_debug("Player within range")
 		player_position = area.global_position
 		is_player_within_range = true
@@ -40,7 +40,7 @@ func _on_detection_area_area_entered(area):
 
 
 func _on_detection_area_area_exited(area):
-	if area.name == "PlayerHitbox":
+	if area.name == "PlayerHitbox" and health > 0:
 		#print_debug("Player exited range")
 		is_player_within_range = false
 		$Timer.set_paused(true)
@@ -50,14 +50,22 @@ func _on_detection_area_area_exited(area):
 
 func _on_enemy_damage_hitbox_area_entered(area):
 	if area.name == "MeleeHitbox" or area.name == "GroundPoundHitbox":
-		$AnimatedSprite2D.play("damaged")
-		print_debug(name + " took damage!")
-		if (health <= 1):
-			$AnimatedSprite2D.play("death")
+		_shooting_take_damage()
+
+func _shooting_take_damage():
+	health -= 1
+	print_debug(name + " took damage!")
+	
+	if (health <= 0):
+		$AnimatedSprite2D.play("death")
 		await $AnimatedSprite2D.animation_finished
-		_take_damage()
+		queue_free()
+	else:
+		$AnimatedSprite2D.play("damaged")
+	pass
 
 func _on_timer_timeout():
 	#print_debug("Shooting player")
-	_shoot_player()
+	if health >= 0:
+		_shoot_player()
 	pass # Replace with function body.
